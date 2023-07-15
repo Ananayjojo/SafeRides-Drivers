f52e21c7018fc14cc617300083e179f9c4c91e45
Ananay Joshi <ananayjo22@gmail.com>
2023-07-15 16:04:11 -0700

    First commit

---
+++ components/Maps.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Maps = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  )
+}
+
+export default Maps
+// this file is for the map functionality which is respondible for recieving a location 
+//and direct the driver to said destination
+// add this to the home page 
+
+const styles = StyleSheet.create({})
